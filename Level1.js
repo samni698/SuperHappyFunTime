c59607debcb8c3c9c38e6a0efc2aa61777913710
Samuel Nilsson <samni698@gmail.com>
2014-09-29 11:32:58 +0200

    Changed level1 to an easy starting level.

--- Level1.js
+++ Level1.js
@@ -1,9 +1,12 @@
 Level1 = function() {};
 
-Level1.prototype = new Level(1600, 600);
+Level1.prototype = new Level(1600, 800);
 
 Level1.prototype.loadLevelObjects = function() {
 
 	this.addPlatform(0, this.world.height - 64, 800, 64);
 
+	this.addPlatform(900, this.world.height - 400, 300, 40);
+
+	this.addPlatform(1200, this.world.height - 64, 400, 64);
 };
